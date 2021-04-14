101ec116d4087e39bd2fa42225da0cb493555be8
jackykwandesign <jackykwandesign@gmail.com>
2021-04-14 12:46:52 +0800

    update with remote blockchain setup

--- truffle-config.js
+++ truffle-config.js
@@ -7,9 +7,10 @@ module.exports = {
   //
   networks: {
    development: {
-     host: "127.0.0.1",
+     host: "34.206.228.39",
      port: 7545,
-     network_id: "*"
+     network_id: "*",
+     gasPrice: 120000000000,
    },
   },
   compilers: {
